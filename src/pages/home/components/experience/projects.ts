c29f662b19c980480321f5ef1644804af2eb922e
armandojes <armandodejesus678@gmail.com>
2022-10-20 13:31:38 -0500

    update

---
+++ src/pages/home/components/experience/projects.ts
@@ -0,0 +1,106 @@
+import smneMain from 'assets/projects/smne_home.png';
+
+const projects = [
+  {
+    title: 'Algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+    description: [
+      'La herramienta digital para consultar los algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+      'El objetivo de esta herramienta de trabajo es poder ofrecerle a los pacientes un diagnóstico eficiente y oportuno.',
+    ],
+    mainPictire: smneMain,
+    url: 'https://smne-beta.web.app/',
+    github: null,
+  },
+  {
+    title: 'Algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+    description: [
+      'La herramienta digital para consultar los algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+      'El objetivo de esta herramienta de trabajo es poder ofrecerle a los pacientes un diagnóstico eficiente y oportuno.',
+    ],
+    mainPictire: smneMain,
+    url: 'https://smne-beta.web.app/',
+    github: null,
+  },
+  {
+    title: 'Algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+    description: [
+      'La herramienta digital para consultar los algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+      'El objetivo de esta herramienta de trabajo es poder ofrecerle a los pacientes un diagnóstico eficiente y oportuno.',
+    ],
+    mainPictire: smneMain,
+    url: 'https://smne-beta.web.app/',
+    github: null,
+  },
+  {
+    title: 'Algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+    description: [
+      'La herramienta digital para consultar los algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+      'El objetivo de esta herramienta de trabajo es poder ofrecerle a los pacientes un diagnóstico eficiente y oportuno.',
+    ],
+    mainPictire: smneMain,
+    url: 'https://smne-beta.web.app/',
+    github: null,
+  },
+  {
+    title: 'Algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+    description: [
+      'La herramienta digital para consultar los algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+      'El objetivo de esta herramienta de trabajo es poder ofrecerle a los pacientes un diagnóstico eficiente y oportuno.',
+    ],
+    mainPictire: smneMain,
+    url: 'https://smne-beta.web.app/',
+    github: null,
+  },
+  {
+    title: 'Algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+    description: [
+      'La herramienta digital para consultar los algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+      'El objetivo de esta herramienta de trabajo es poder ofrecerle a los pacientes un diagnóstico eficiente y oportuno.',
+    ],
+    mainPictire: smneMain,
+    url: 'https://smne-beta.web.app/',
+    github: null,
+  },
+  {
+    title: 'Algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+    description: [
+      'La herramienta digital para consultar los algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+      'El objetivo de esta herramienta de trabajo es poder ofrecerle a los pacientes un diagnóstico eficiente y oportuno.',
+    ],
+    mainPictire: smneMain,
+    url: 'https://smne-beta.web.app/',
+    github: null,
+  },
+  {
+    title: 'Algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+    description: [
+      'La herramienta digital para consultar los algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+      'El objetivo de esta herramienta de trabajo es poder ofrecerle a los pacientes un diagnóstico eficiente y oportuno.',
+    ],
+    mainPictire: smneMain,
+    url: 'https://smne-beta.web.app/',
+    github: null,
+  },
+  {
+    title: 'Algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+    description: [
+      'La herramienta digital para consultar los algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+      'El objetivo de esta herramienta de trabajo es poder ofrecerle a los pacientes un diagnóstico eficiente y oportuno.',
+    ],
+    mainPictire: smneMain,
+    url: 'https://smne-beta.web.app/',
+    github: null,
+  },
+  {
+    title: 'Algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+    description: [
+      'La herramienta digital para consultar los algoritmos para el tratamiento farmacológico de la hiperglucemia en diabetes tipo 2',
+      'El objetivo de esta herramienta de trabajo es poder ofrecerle a los pacientes un diagnóstico eficiente y oportuno.',
+    ],
+    mainPictire: smneMain,
+    url: 'https://smne-beta.web.app/',
+    github: null,
+  },
+];
+
+export default projects;
